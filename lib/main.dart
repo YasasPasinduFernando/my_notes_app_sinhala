70cebc1c510eeb0827f38eba3853ebbda3537dde
yasaspasindufernando <yasaspasindufernando@gmail.com>
2024-12-17 09:37:16 +0530

    make simple note app

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,190 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Yasas',
+      title: 'සටහන් පොත',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.green,
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'yasas Page'),
+      home: const NotesHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Note {
+  String title;
+  String content;
+  DateTime dateTime;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Note({required this.title, required this.content, required this.dateTime});
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class NotesHomePage extends StatefulWidget {
+  const NotesHomePage({super.key});
 
-  void _incrementCounter() {
+  @override
+  State<NotesHomePage> createState() => _NotesHomePageState();
+}
+
+class _NotesHomePageState extends State<NotesHomePage> {
+  List<Note> notes = [];
+
+  void _addNewNote() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String newTitle = '';
+        String newContent = '';
+
+        return AlertDialog(
+          title: const Text('නව සටහන'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: const InputDecoration(hintText: 'මාතෘකාව'),
+                onChanged: (value) {
+                  newTitle = value;
+                },
+              ),
+              TextField(
+                decoration: const InputDecoration(hintText: 'සටහන'),
+                maxLines: 3,
+                onChanged: (value) {
+                  newContent = value;
+                },
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('අවලංගු කරන්න'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  notes.add(Note(
+                    title: newTitle,
+                    content: newContent,
+                    dateTime: DateTime.now(),
+                  ));
+                });
+                Navigator.pop(context);
+              },
+              child: const Text('සුරකින්න'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deleteNote(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      notes.removeAt(index);
     });
+  }
+
+  void _editNote(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String editedTitle = notes[index].title;
+        String editedContent = notes[index].content;
+
+        return AlertDialog(
+          title: const Text('සටහන සංස්කරණය කරන්න'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: const InputDecoration(hintText: 'මාතෘකාව'),
+                controller: TextEditingController(text: editedTitle),
+                onChanged: (value) {
+                  editedTitle = value;
+                },
+              ),
+              TextField(
+                decoration: const InputDecoration(hintText: 'සටහන'),
+                controller: TextEditingController(text: editedContent),
+                maxLines: 3,
+                onChanged: (value) {
+                  editedContent = value;
+                },
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('අවලංගු කරන්න'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  notes[index].title = editedTitle;
+                  notes[index].content = editedContent;
+                });
+                Navigator.pop(context);
+              },
+              child: const Text('යාවත්කාලීන කරන්න'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('මගේ සටහන් පොත'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: notes.isEmpty
+          ? const Center(
+              child: Text('තවම සටහන් නැත'),
+            )
+          : ListView.builder(
+              itemCount: notes.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: Text(
+                      notes[index].title,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(notes[index].content),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.edit),
+                          onPressed: () => _editNote(index),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () => _deleteNote(index),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _addNewNote,
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
